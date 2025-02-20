b9b39957aa2bf45dc5d7ddcbbc799302181406de
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-20 16:39:56 -0400

    Uso de constantes

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php 
-$nombre = "Manuel Henriquez";
-?>
+//Nombre de la constante y valor
+define('CURSO', 'PHP 8 desde cero');
+define('USUARIO_1', 'Manuel Henriquez');
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso de PHP 8 - Bases</title>
-</head>
-<body>
-    <h1><?php echo "Hola mundo, mi nombre es $nombre"; ?></h1>
-</body>
-</html>
+//PHP 8 permite definir constantes arrays
+define('ANIMALES', [
+    'Perro',
+    'Gato',
+    'Castor',
+    'León',
+]);
+
+//La constante no necesita el prefijo $
+echo CURSO . '. Alumno ' . USUARIO_1;
+echo '<br>';
+echo ANIMALES[0];
+
+echo '<br>';
+//Comprobar si una constante existe
+if (defined('CURSO')) {
+    echo 'Está definido';
+}
+else {
+    echo 'No está definido';
+}
+
+echo '<br>';
+//Constantes predefinicas
+echo 'Mi versión de php es: ' . PHP_VERSION;
+echo '<br>';
+echo 'Mi SO de php es: ' . PHP_OS;
