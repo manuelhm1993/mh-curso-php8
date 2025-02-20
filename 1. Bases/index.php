0d3fd86f3289c8371c48c5347b64918119602c13
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-20 14:57:35 -0400

    Inicio del curso PHP 8, primer programa PHP

---
+++ 1. Bases/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP 8 - Bases</title>
+</head>
+<body>
+    <h1><?php echo "Hola mundo"; ?></h1>
+</body>
+</html>
