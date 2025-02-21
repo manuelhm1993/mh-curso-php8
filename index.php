5b56a1e87feb9faba3955801a8e0cab119167a72
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-21 10:21:15 -0400

    Operador ternario, permite resumir una estructura if en una línea de código

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 $a = 5;
-$b = 5;
+$b = 51;
 
+/*
 if ($a > $b) {
     echo "El valor de a es mayor que el valor de b";
 }
@@ -10,4 +11,7 @@ elseif ($a < $b) {
 } 
 else {
     echo "El valor de a es igual que el valor de b";
-}
+}*/
+
+//El operador ternario debe colocarse entre paréntesis si se valida entre una cadena
+echo "El valor de a es" . (($a > $b) ? " mayor " : (($a < $b) ? " menor " : " igual ")) . "que el valor de b";
