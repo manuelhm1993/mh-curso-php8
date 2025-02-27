c982ebf2e2ebb5e97db441c09145a478bc518891
Manuel Henriquez <manuelhm1993@gmail.com>
2025-02-27 15:08:32 -0400

    Bucle while y do while, estructuras repetitivas

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php 
-$a = 5;
-$b = 51;
 
-/*
-if ($a > $b) {
-    echo "El valor de a es mayor que el valor de b";
+$base = 5;
+$exponente = 4;
+$resultado = 1;
+$i = 0;
+
+while($i < $exponente)
+{
+    $resultado *= $base;
+    $i++;
 }
-elseif ($a < $b) {
-    echo "El valor de a es menor que el valor de b";
-} 
-else {
-    echo "El valor de a es igual que el valor de b";
-}*/
 
-//El operador ternario debe colocarse entre paréntesis si se valida entre una cadena
-echo "El valor de a es" . (($a > $b) ? " mayor " : (($a < $b) ? " menor " : " igual ")) . "que el valor de b";
+echo "{$base} ^ {$exponente} = {$resultado}";
