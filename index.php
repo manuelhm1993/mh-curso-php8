69a27a38970216921ac6b957dcc8805f572a598c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-11 10:45:02 -0400

    Array multidimensional

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php 
 
-//Array asociativo, clave => valor
-$usuario = [
-    'nombre'   => 'Manuel Henriquez',
-    'email'    => 'manuelhm1993@gmail.com',
-    'telefono' => '04246827377',
+//Array multidimensional
+$usuarios = [
+    [
+        'nombre'    => 'Manuel Henriquez',
+        'email'     => 'manuelhm1993@gmail.com',
+        'telefono'  => '04246827377',
+        'direccion' => [
+            'pais'   => 'Venezuela',
+            'ciudad' => 'Maracaibo',
+        ]
+    ],
+    [
+        'nombre'   => 'Luna Lopez',
+        'email'    => 'lunaisalp@gmail.com',
+        'telefono' => '0414-6939793',
+    ],
+    [
+        'nombre'   => 'Mayra Moreno',
+        'email'    => 'mayra.moreno1959@gmail.com',
+        'telefono' => '04246213686',
+    ],
 ];
     
-echo $usuario['nombre'];
+echo $usuarios[0]['direccion']['ciudad'];
