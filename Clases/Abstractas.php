ceaa8a87ac3629524f07238117aaa3b7fdfc8ed8
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-12 14:15:20 -0400

    Las clases abstractas permiten ampliar la definición de un método

--- Clases/Abstractas.php
+++ Clases/Abstractas.php
@@ -4,7 +4,7 @@
 abstract class Abstracta 
 {
     protected abstract function getValor(): string;
-    protected abstract function valorPrefijo($valor): void;
+    public abstract function valorPrefijo(string $valor): string;
 
     public function imprimir(): void
     {
@@ -14,13 +14,16 @@ abstract class Abstracta
 
 class NuevaClase extends Abstracta
 {
+    private string $message = 'Luna';
+
     protected function getValor(): string
     {
         return "Hola mundo";
     }
 
-    protected function valorPrefijo($valor): void
+    //Siempre que respete la estructura principal, se puede modificar a gusto
+    public function valorPrefijo(string $valor, string $separador = ' '): string
     {
-        
+        return "{$valor}{$separador}{$this->message}";
     }
 }
--- index.php
+++ index.php
@@ -5,3 +5,5 @@ require_once './Clases/Abstractas.php';
 $obj = new NuevaClase;
 
 $obj->imprimir();
+echo "<br>";
+echo $obj->valorPrefijo('Buena suerte');
