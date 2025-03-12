666c036aa0a2b36e17e7058c61adfed288398ae4
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-12 15:18:25 -0400

    Cambiar la visibilidad de un método para un trait

--- Clases/Traits.php
+++ Clases/Traits.php
@@ -5,24 +5,16 @@ require_once './Clases/Persona.php';
 //Conjunto de funciones extra que se implementan en una clase
 trait A 
 {
-    public function saludo(): void
+    protected function saludo(): void
     {
         echo "Hola mundo desde el trait A";
     }
 }
 
-trait B
-{
-    public function saludo(): void
-    {
-        echo "Hola mundo desde el trait B";
-    }
-}
-
 class Venezolano extends Persona //Herencia
 {
-    //Si dos métodos tienen el mismo nombre se debe especificar cuál usar
-    use A, B {
-        B::saludo insteadOf A;
+    //Cambiar la visibilidad de un método del trait
+    use A { 
+        saludo as public; 
     }
 }
