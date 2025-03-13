a0eb318c8b361638a3b4c0d0e7eff594d0a45f8f
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-13 09:15:46 -0400

    Uso de variables y métodos static

--- POO/UsoStatic.php
+++ POO/UsoStatic.php
@@ -4,12 +4,26 @@ namespace POO;
 
 class UsoStatic
 {
+    private static string $nombre;
+
     //El método static le pertenece a la clase y no a una instancia
     public static function saludar(): void
     {
         echo "Hola mundo";
     }
+
+    public static function getNombre(): string
+    {
+        return self::$nombre;
+    }
+
+    public static function setNombre(string $nombre): void
+    {
+        self::$nombre = $nombre;
+    }
 }
 
 //Se usa directamente la clase para invocar al método
-UsoStatic::saludar();
+UsoStatic::setNombre('Manuel Henriquez');
+
+echo UsoStatic::getNombre();
