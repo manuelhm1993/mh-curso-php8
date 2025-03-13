ee720cf13c1f2f399f1f8cdaebcf29361525b003
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-13 15:19:22 -0400

    Comandos DDL para crear y eliminar objetos de la DB

---
+++ MySQL/SQL/Comandos DDL/Create table.sql
@@ -0,0 +1,11 @@
+create table `ejemplo`.`contactos`(
+	`id` int not null auto_increment,
+    `nombre` varchar(64) not null,
+    `apellidos` varchar(64) not null,
+    `direccion` varchar(255) not null,
+    `telefono` varchar(11) not null,
+    `correo` varchar(255) not null,
+    `fecha_nacimiento` date default null,
+    `familia` bit default null,
+    primary key (`id`)
+);
