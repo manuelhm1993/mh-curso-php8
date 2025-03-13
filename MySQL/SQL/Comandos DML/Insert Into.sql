2fe1020c91880ce64f5bd8cf00e7d374b56d4a2f
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-13 15:19:52 -0400

    Comandos DML para manipular la información insertando o actualizando

---
+++ MySQL/SQL/Comandos DML/Insert Into.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ejemplo`.`contactos` 
+(`nombre`, `apellidos`, `direccion`, `telefono`, `correo`, `fecha_nacimiento`, `familia`) 
+VALUES 
+('Manuel', 'Henriquez Moreno', 'Calle falsa 123  ', '04241234567', 'manuel@prueba.com', '1993-07-23', b'1'),
+('Luna', 'Lopez', 'Calle falsa 123  ', '04241234568', 'luna@prueba.com', '1998-02-27', b'1');
